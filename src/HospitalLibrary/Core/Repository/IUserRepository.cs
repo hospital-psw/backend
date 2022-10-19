c12bdbe6cdd34ec584a22f9ec0c1a6d74097ed6b
Andrija25 <93194317+Andrija25@users.noreply.github.com>
2022-10-19 15:35:37 +0200

    IUserRepository and UserRepository added

---
+++ src/HospitalLibrary/Core/Repository/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace HospitalLibrary.Core.Repository
+{
+    using HospitalLibrary.Core.Model;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IUserRepository : IBaseRepository<User>
+    {
+        
+        
+    }
+}
---
+++ src/HospitalLibrary/Core/Repository/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace HospitalLibrary.Core.Repository
+{
+    using HospitalLibrary.Core.Model;
+    using HospitalLibrary.Settings;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class UserRepository : BaseRepository<User>,IUserRepository
+    {
+        public UserRepository(HospitalDbContext context) : base(context) { }
+    }
+}
