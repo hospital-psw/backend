141e0d61d803627fc6e035eb193647ee99d672ad
Ratko Ružičić <ruzicic.ra68.2019@uns.ac.rs>
2022-10-24 16:44:50 +0200

    Add syntax error

--- src/IntegrationLibrary/Core/Model/BloodBank.cs
+++ src/IntegrationLibrary/Core/Model/BloodBank.cs
@@ -8,7 +8,7 @@ namespace IntegrationLibrary.Core.Model
 {
     public class BloodBank : Entity
     {
-        public string Name { get; set; }
+        public string Name    { get; set; }
         public string Email { get; set; }
         public string ApiUrl { get; set; }
         public string ApiKey { get; set; }
