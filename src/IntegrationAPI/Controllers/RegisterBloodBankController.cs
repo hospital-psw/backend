1e0e27a37d65d84c26097243fb7cd3eaa6d89bb4
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2022-10-25 14:13:59 +0200

    Add endpoint with post request for registering to DB, sending mail and API key

---
+++ src/IntegrationAPI/Controllers/RegisterBloodBankController.cs
@@ -0,0 +1,59 @@
+﻿namespace IntegrationAPI.Controllers
+{
+    using IntegrationLibrary.Core.Model;
+    using IntegrationLibrary.Core.Service;
+    using IntegrationLibrary.Core.Service.Core;
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using System.Security.Cryptography;
+    using System.Text;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RegisterBloodBankController
+    {
+        private readonly BloodBankService _bloodBankService;
+
+        public RegisterBloodBankController(IBloodBankService bloodBankService)
+        {
+            _bloodBankService = (BloodBankService)bloodBankService;
+        }
+
+        private string ByteArrToString(byte[] byteArr)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < byteArr.Length; i++)
+            {
+                sb.Append(byteArr[i].ToString("X2"));
+            }
+
+            return sb.ToString();
+        }
+
+        private string GenerateAPIKey(string mail)
+        {
+            string current_date = DateTime.Now.ToString();
+            string hash_source = mail + current_date;
+
+            byte[] byteSource = ASCIIEncoding.ASCII.GetBytes(hash_source);
+
+            var md5 = new HMACMD5();
+            byte[] byteHash = md5.ComputeHash(byteSource);
+            string apiKey = ByteArrToString(byteHash);
+
+            return apiKey;
+        }
+
+        [HttpPost]
+        public string RegisterBloodBank(BloodBank bloodBank)
+        {
+            string api_key = GenerateAPIKey(bloodBank.Email);
+            bloodBank.ApiKey = api_key;
+
+            MailingService.SendEmail(bloodBank.Email, bloodBank.ApiKey);
+            _bloodBankService.Add(bloodBank);
+
+            return "Generated key: " + api_key;
+        }
+    }
+}
--- src/IntegrationAPI/Startup.cs
+++ src/IntegrationAPI/Startup.cs
@@ -1,3 +1,4 @@
+using IntegrationAPI.Middleware;
 using IntegrationLibrary.Core.Service;
 using IntegrationLibrary.Core.Service.Core;
 using Microsoft.AspNetCore.Builder;
@@ -6,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using System.Collections.Generic;
 
 namespace IntegrationAPI
 {
@@ -48,6 +50,11 @@ namespace IntegrationAPI
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "IntegrationAPI v1"));
             }
+
+            app.UseMiddleware<APIKeyMiddleware>(new APIKeyOptions
+            {
+                Endpoints = new List<string> { @"/BloodBank" }
+            });
 
             app.UseRouting();
 
