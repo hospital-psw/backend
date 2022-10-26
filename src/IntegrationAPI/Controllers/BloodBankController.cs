88fa5ff3cbad91cb47bddefa32c36870b9e6bc0b
Ratko Ružičić <ruzicic.ra68.2019@uns.ac.rs>
2022-10-26 10:22:19 +0200

    Remove generic base controller and add DTO to update endpoint

--- src/IntegrationAPI/Controllers/BloodBankController.cs
+++ src/IntegrationAPI/Controllers/BloodBankController.cs
@@ -1,19 +1,98 @@
 ﻿namespace IntegrationAPI.Controllers
 {
+    using AutoMapper;
+    using IntegrationAPI.DTO;
     using IntegrationLibrary.Core.Model;
-    using IntegrationLibrary.Core.Service;
     using IntegrationLibrary.Core.Service.Core;
     using Microsoft.AspNetCore.Mvc;
 
     [ApiController]
     [Route("api/[controller]")]
-    public class BloodBankController : BaseController<BloodBank>
+    public class BloodBankController : ControllerBase
     {
         private readonly IBloodBankService _bloodBankService;
+        private readonly IMapper _mapper;
 
-        public BloodBankController(IBloodBankService bloodBankService)
+        public BloodBankController(IBloodBankService bloodBankService, IMapper mapper)
         {
             _bloodBankService = bloodBankService;
+            _mapper = mapper;
         }
+
+        [HttpGet("all")]
+        public IActionResult GetAll()
+        {
+            return Ok(_bloodBankService.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public virtual IActionResult Get(int id)
+        {
+            BloodBank entity = _bloodBankService.Get(id);
+
+            if (entity == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(entity);
+        }
+
+        [HttpPost]
+        public IActionResult Add(BloodBank bloodBank)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (bloodBank == null)
+            {
+                return BadRequest();
+            }
+
+            BloodBank response = _bloodBankService.Create(bloodBank);
+
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public IActionResult Update(UpdateBloodBankDTO bloodBank)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (bloodBank == null)
+            {
+                return BadRequest();
+            }
+
+            var originalBloodBank = _bloodBankService.Get(bloodBank.Id);
+            if (originalBloodBank == null)
+            {
+                return BadRequest();
+            }
+            var updatedBloodBank = _mapper.Map<BloodBank>(bloodBank);
+            updatedBloodBank.ApiKey = originalBloodBank.ApiKey; 
+
+            var responseEntity = _bloodBankService.Update(updatedBloodBank);
+
+            return Ok(responseEntity);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            bool response = _bloodBankService.Delete(id);
+            if (!response)
+            {
+                return BadRequest(response);
+            }
+
+            return NoContent();
+        }
+
     }
 }
---
+++ src/IntegrationAPI/DTO/UpdateBloodBankDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace IntegrationAPI.DTO
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class UpdateBloodBankDTO
+    {
+        [Required]
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        public string ApiUrl { get; set; }
+        public string GetBloodTypeAvailability { get; set; }
+        public string GetBloodTypeAndAmountAvailability { get; set; }
+    }
+}
