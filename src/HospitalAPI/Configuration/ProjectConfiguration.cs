01505984e3cd9c961ba9fdd43a59ded64a1e8212
Gravarica <93195018+Gravarica@users.noreply.github.com>
2022-10-27 00:28:03 +0200

    Added email configuration to app settings json file

--- src/HospitalAPI/Configuration/ProjectConfiguration.cs
+++ src/HospitalAPI/Configuration/ProjectConfiguration.cs
@@ -19,4 +19,15 @@
         public string Audience { get; set; }
         public string Subject { get; set; }
     }
+
+    public class EmailSettings
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int Port { get; set; }
+        public string FromEmail { get; set; }
+        public string DisplayName { get; set; }
+        public string ServerAddress { get; set; }
+        public bool EnableSsl { get; set; }
+    }
 }
