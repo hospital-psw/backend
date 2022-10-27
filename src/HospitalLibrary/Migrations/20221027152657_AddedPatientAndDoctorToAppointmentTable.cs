bc7b9c25936692d458987d7edfd2790ae2e06a73
Vuk Milanovic <vuk.milanovic11@gmial.com>
2022-10-27 17:30:46 +0200

    Database updated with Patient & Doctor columns in Appointment table

---
+++ src/HospitalLibrary/Migrations/20221027152657_AddedPatientAndDoctorToAppointmentTable.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HospitalLibrary.Migrations
+{
+    public partial class AddedPatientAndDoctorToAppointmentTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.AddColumn<int>(
+                name: "DoctorId",
+                table: "Appointments",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PatientId",
+                table: "Appointments",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_DoctorId",
+                table: "Appointments",
+                column: "DoctorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_PatientId",
+                table: "Appointments",
+                column: "PatientId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Appointments_Users_DoctorId",
+                table: "Appointments",
+                column: "DoctorId",
+                principalTable: "Users",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Appointments_Users_PatientId",
+                table: "Appointments",
+                column: "PatientId",
+                principalTable: "Users",
+                principalColumn: "Id");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Appointments_Users_DoctorId",
+                table: "Appointments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Appointments_Users_PatientId",
+                table: "Appointments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Appointments_DoctorId",
+                table: "Appointments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Appointments_PatientId",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "DoctorId",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "PatientId",
+                table: "Appointments");
+
+        }
+    }
+}
