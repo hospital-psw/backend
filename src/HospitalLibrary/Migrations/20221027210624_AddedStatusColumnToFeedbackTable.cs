674263bcb79e6ab3d6deea912f91ec9594625c57
tkrgovic <tasakrgovic@gmail.com>
2022-10-27 23:11:31 +0200

    Added migration feedback

---
+++ src/HospitalLibrary/Migrations/20221027210624_AddedStatusColumnToFeedbackTable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HospitalLibrary.Migrations
+{
+    public partial class AddedStatusColumnToFeedbackTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Feedback",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Feedback");
+        }
+    }
+}
