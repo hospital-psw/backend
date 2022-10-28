52a5128e149ddf7497c81bfc34422dcecfb44aa4
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2022-10-28 10:46:26 +0200

    Add password generation to SecretGenerator

---
+++ src/IntegrationLibrary/Util/SecretGenerator.cs
@@ -0,0 +1,23 @@
+﻿namespace IntegrationLibrary.Util
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class SecretGenerator
+    {
+        public static string generateRandomPassword(int pwLength = 15)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            Random random = new Random();
+
+            return new string(
+                Enumerable.Repeat(chars, pwLength)
+                .Select(s => s[random.Next(s.Length)])
+                .ToArray()
+                );
+        }
+    }
+}
