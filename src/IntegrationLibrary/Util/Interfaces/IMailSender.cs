fd5fab6a2f2fe598f1ae1fe4d5962e4a849231a7
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2022-10-28 11:03:57 +0200

    Add mailer interface

---
+++ src/IntegrationLibrary/Util/Interfaces/IMailSender.cs
@@ -0,0 +1,15 @@
+﻿namespace IntegrationLibrary.Util.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IMailSender
+    {
+        Task RunAsync(string template);
+
+        void SendEmail(string template);
+    }
+}
