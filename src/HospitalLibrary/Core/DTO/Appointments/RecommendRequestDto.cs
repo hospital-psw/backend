1e121378123a6a675f011140e078027248aae4ec
Galin-Ilija <ikiakus@gmail.com>
2022-10-29 20:43:55 +0200

    Added controller endpoints

---
+++ src/HospitalLibrary/Core/DTO/Appointments/RecommendRequestDto.cs
@@ -0,0 +1,17 @@
+﻿namespace HospitalLibrary.Core.DTO.Appointments
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class RecommendRequestDto
+    {
+        public DateTime Date { get; set; }
+
+        public int PatientId { get; set; }
+
+        public int DoctorId { get; set; }
+    }
+}
