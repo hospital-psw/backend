11c67c4fe1612995eb566544ea2bfe049bd2fa42
unknown <kalinic.ra71.2019@uns.ac.rs>
2022-10-31 19:36:12 +0100

    fix small bugs and refactoring check blood type feature

---
+++ src/IntegrationLibrary/Util/BBConnections.cs
@@ -0,0 +1,23 @@
+namespace IntegrationLibrary.Util
+{
+    using IntegrationLibrary.BloodBank;
+    using IntegrationLibrary.Util.Interfaces;
+    using System;
+    using System.Net.Http;
+
+    public class BBConnections: IBBConnections
+    {
+        public bool SendHttpRequestToBank(BloodBank bloodBank, string type)
+        {
+            using (var client = new HttpClient())
+            {
+                var endpoint = new Uri($"http://{bloodBank.ApiUrl}/{bloodBank.GetBloodTypeAvailability.Replace("!BLOOD_TYPE", type)}");
+                client.DefaultRequestHeaders.Add("X-API-KEY", bloodBank.ApiKey);
+                var result = client.GetAsync(endpoint).Result;
+                var json = result.Content.ReadAsStringAsync().Result;
+                return Convert.ToBoolean(json);
+            }
+        }
+    }
+
+}
