900a9eb022781088d8eb5c71a96178d2244e7b25
DarkoCokic72 <93010785+DarkoCokic72@users.noreply.github.com>
2022-11-09 20:56:03 +0100

    Added model for VacationRequest

---
+++ src/HospitalLibrary/Core/Model/VacationRequest/VacationRequest.cs
@@ -0,0 +1,38 @@
+﻿namespace HospitalLibrary.Core.Model.VacationRequest
+{
+    using HospitalLibrary.Core.Model.Enums;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class VacationRequest
+    {
+        public Doctor Doctor { get; set; }
+
+        public DateTime From { get; set; }
+
+        public DateTime To { get; set; }
+
+        public VacationRequestStatus Status { get; set; }
+
+        public String Reason { get; set; }
+
+        public String RejectionReason { get; set; }
+
+        public int Id { get; set; }
+
+        public VacationRequest() { }
+
+        public VacationRequest(Doctor doctor, DateTime from, DateTime to, VacationRequestStatus status, string reason, string rejectionReason)
+        {
+            Doctor = doctor;
+            From = from;
+            To = to;
+            Status = status;
+            Reason = reason;
+            RejectionReason = rejectionReason;
+        }
+    }
+}
