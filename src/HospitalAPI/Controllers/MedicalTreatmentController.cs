bfe238c104f7b177dcbaf68c1b125c287c603562
Vuk Milanovic <vuk.milanovic11@gmial.com>
2022-11-11 19:47:28 +0100

    Bug solved

--- src/HospitalAPI/Controllers/MedicalTreatmentController.cs
+++ src/HospitalAPI/Controllers/MedicalTreatmentController.cs
@@ -1,8 +1,13 @@
 ﻿namespace HospitalAPI.Controllers
 {
+    using HospitalAPI.Dto.MedicamentTreatment;
+    using HospitalAPI.Mappers.MedicalTreatment;
+    using HospitalLibrary.Core.DTO.MedicalTreatment;
     using HospitalLibrary.Core.Model.MedicalTreatment;
     using HospitalLibrary.Core.Service.Core;
     using Microsoft.AspNetCore.Mvc;
+    using System.Collections.Generic;
+    using System.Linq;
 
     [ApiController]
     [Route("api/[controller]")]
@@ -15,5 +20,49 @@
         {
             _medicalTreatmentService = medicalTreatmentService;
         }
+
+        [HttpPost]
+        public IActionResult Create(NewMedicalTreatmentDto dto)
+        {
+            if (dto == null)
+            {
+                BadRequest("Bad request, please enter valid data.");
+            }
+            else if (dto.PatientId == default(int) || dto.DoctorId == default(int) || dto.RoomId == default(int))
+            {
+                BadRequest("Bad request, please enter valid data.");
+            }
+
+            return Ok(_medicalTreatmentService.Add(dto));
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            MedicalTreatment medicalTreatment = _medicalTreatmentService.Get(id);
+
+            if (medicalTreatment == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(MedicalTreatmentMapper.EntityToEntityDto(medicalTreatment));
+        }
+
+        [HttpGet("all")]
+        public IActionResult GetAll()
+        {
+            List<MedicalTreatment> medicalTreatments = _medicalTreatmentService.GetAll().ToList();
+            List<MedicalTreatmentDto> dtoList = new List<MedicalTreatmentDto>();
+
+            if (medicalTreatments == null)
+            {
+                return NotFound();
+            }
+
+            medicalTreatments.ForEach(mt => dtoList.Add(MedicalTreatmentMapper.EntityToEntityDto(mt)));
+            return Ok(dtoList);
+        }
     }
+
 }
