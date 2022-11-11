6486c3241761fbb2bb58718f01cbda38ae3eb399
Ratko Ružičić <ruzicic.ra68.2019@uns.ac.rs>
2022-11-11 21:11:54 +0100

    Inject unit of work instead of manually instantianting it

--- src/IntegrationLibrary/Core/IUnitOfWork.cs
+++ src/IntegrationLibrary/Core/IUnitOfWork.cs
@@ -1,9 +1,11 @@
 ﻿namespace IntegrationLibrary.Core
 {
+    using IntegrationLibrary.BloodBank.Interfaces;
     using System;
 
     public interface IUnitOfWork : IDisposable
     {
+        public IBloodBankRepository BloodBankRepository { get; }
         int Save();
     }
 }
