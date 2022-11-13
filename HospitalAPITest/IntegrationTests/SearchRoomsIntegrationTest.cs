c4a90bfa1efe2eb6cb167919fc4063d7c9cecfbf
BrankaKljajic <branka.kljajic00@gmail.com>
2022-11-13 02:30:50 +0100

    Started creating integration test for searching rooms

---
+++ HospitalAPITest/IntegrationTests/SearchRoomsIntegrationTest.cs
@@ -0,0 +1,30 @@
+﻿namespace HospitalAPITest.IntegrationTests
+{
+    using HospitalAPI.Controllers;
+    using HospitalAPITest.Setup;
+    using HospitalLibrary.Core.Service.Core;
+    using Microsoft.Extensions.DependencyInjection;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class SearchRoomsIntegrationTest : BaseIntegrationTest
+    {
+        public SearchRoomsIntegrationTest(TestDatabaseFactory factory) : base(factory)
+        {
+        }
+
+        private static RoomsController SetupController(IServiceScope serviceScope)
+        {
+            return new RoomsController(serviceScope.ServiceProvider.GetRequiredService<IRoomService>());
+        }
+
+        [Fact]
+        public void Get_searched_rooms()
+        {
+            
+        }
+    }
+}
