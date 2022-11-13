16f01f3e867b10e7d558b169b512e32128174356
Ana Vulin <vulin.ra65.2019@uns.ac.rs>
2022-11-13 01:17:38 +0100

    Created endpoint for recommended relocation appointments

--- src/HospitalAPI/Controllers/RelocationController.cs
+++ src/HospitalAPI/Controllers/RelocationController.cs
@@ -1,17 +1,24 @@
 ﻿namespace HospitalAPI.Controllers
 {
+    using HospitalAPI.Dto;
     using HospitalLibrary.Core.Service.Core;
     using Microsoft.AspNetCore.Mvc;
 
     [ApiController]
     [Route("api/[controller]")]
-    public class RelocationController
+    public class RelocationController : ControllerBase
     {
         private IRelocationService _relocationService;
 
         public RelocationController(IRelocationService relocationService)
         {
             _relocationService = relocationService;
+        }
+
+        [HttpGet("getRecommendedAppointments")]
+        public IActionResult GetRecommendedRelocationAppointments(RecommendRelocationRequestDto dto)
+        {
+            return Ok(_relocationService.GetAvailableAppointments(dto.FromRoom.Id, dto.ToRoom.Id, dto.FromTime, dto.ToTime, dto.Duration));
         }
     }
 }
