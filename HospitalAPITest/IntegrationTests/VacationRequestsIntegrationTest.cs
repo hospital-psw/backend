ad7832374634664b57bbcf556c025a0fe2599238
DarkoCokic72 <93010785+DarkoCokic72@users.noreply.github.com>
2022-11-13 18:07:12 +0100

    Made integration test for creating VacationRequest

--- HospitalAPITest/IntegrationTests/VacationRequestsIntegrationTest.cs
+++ HospitalAPITest/IntegrationTests/VacationRequestsIntegrationTest.cs
@@ -4,7 +4,10 @@
     using HospitalAPI.Controllers;
     using HospitalAPI.Dto;
     using HospitalAPITest.Setup;
+    using HospitalLibrary.Core.DTO.VacationRequest;
+    using HospitalLibrary.Core.Model.Enums;
     using HospitalLibrary.Core.Service.Core;
+    using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.DependencyInjection;
     using System;
@@ -35,6 +38,48 @@
             var result = ((ObjectResult)controller.GetAllPending()).Value as List<VacationRequestDto>;
 
             Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void Create_vacation_request() 
+        {
+            using var scope = Factory.Services.CreateScope();
+            var controller = SetupController(scope);
+
+            NewVacationRequestDto dto = new NewVacationRequestDto()
+            {
+                DoctorId = 3,
+                From = new DateTime(2022, 11, 19),
+                To = new DateTime(2022, 12, 20),
+                Status = VacationRequestStatus.WAITING,
+                Comment = "",
+                Urgent = false,
+            };
+
+            var result = ((OkObjectResult)controller.Create(dto)).Value as VacationRequestDto;
+
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void Dates_are_in_wrong_order()
+        {
+            using var scope = Factory.Services.CreateScope();
+            var controller = SetupController(scope);
+
+            NewVacationRequestDto dto = new NewVacationRequestDto()
+            {
+                DoctorId = 3,
+                From = new DateTime(2022, 12, 20) ,
+                To = new DateTime(2022, 11, 19),
+                Status = VacationRequestStatus.WAITING,
+                Comment = "",
+                Urgent = false,
+            };
+
+            var result = controller.Create(dto) as StatusCodeResult;
+
+            Assert.Equal(result.StatusCode, StatusCodes.Status400BadRequest);
         }
     }
 }
