b92fe03c22b899420c93ba4eba43e4765a4be809
unknown <kalinic.ra71.2019@uns.ac.rs>
2022-11-14 19:40:29 +0100

    added migration for blood banks configuration

---
+++ src/IntegrationLibrary/Migrations/20221114182428_AddColumnsToBloodBankTable.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IntegrationLibrary.Migrations
+{
+    public partial class AddColumnsToBloodBankTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Frequently",
+                table: "BloodBanks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ReportFrom",
+                table: "BloodBanks",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ReportTo",
+                table: "BloodBanks",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Frequently",
+                table: "BloodBanks");
+
+            migrationBuilder.DropColumn(
+                name: "ReportFrom",
+                table: "BloodBanks");
+
+            migrationBuilder.DropColumn(
+                name: "ReportTo",
+                table: "BloodBanks"); 
+        }
+    }
+}
