9cd7c533b39b62681c9aca6945696aaee5924450
Sandra <sandra.brkic5@gmail.com>
2022-11-14 20:10:41 +0100

    Return dto list

--- src/HospitalAPI/Mappers/EquipmentMapper.cs
+++ src/HospitalAPI/Mappers/EquipmentMapper.cs
@@ -6,12 +6,13 @@
 
     public class EquipmentMapper
     {
-        public static EquipmentDto EntityToEntityDto(EquipmentDto equipment)
+        public static EquipmentDto EntityToEntityDto(Equipment equipment)
         {
             EquipmentDto dto = new EquipmentDto();
             dto.EquipmentType = equipment.EquipmentType;
             dto.Quantity = equipment.Quantity;
             dto.Id = equipment.Id;
+            dto.Room = RoomMapper.EntityToEntityDto(equipment.Room);
             return dto;
         }
 
@@ -22,6 +23,7 @@
             equipment.EquipmentType = dto.EquipmentType;
             equipment.Quantity = dto.Quantity;
             equipment.Id = dto.Id;
+            equipment.Room = RoomMapper.EntityDtoToEntity(dto.Room);
             return equipment;
         }
     }
