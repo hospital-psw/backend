8f0d5eb02efd0953db348a3d0120beebf903d7d4
Sandra <sandra.brkic5@gmail.com>
2022-11-14 07:57:52 +0100

    Update searchdto

--- src/HospitalAPI/Dto/SearchCriteriaDto.cs
+++ src/HospitalAPI/Dto/SearchCriteriaDto.cs
@@ -10,8 +10,10 @@
         public string RoomPurpose { get; set; }
         public DateTime WorkingHoursStart { get; set; }
         public DateTime WorkingHoursEnd { get; set; }
+        public int EquipmentType { get; set; }
+        public int Quantity { get; set; }
 
-        public SearchCriteriaDto(int buildingId, int floorNumber, string roomNumber, string roomPurpose, DateTime workingHoursStart, DateTime workingHoursEnd)
+        public SearchCriteriaDto(int buildingId, int floorNumber, string roomNumber, string roomPurpose, DateTime workingHoursStart, DateTime workingHoursEnd, int equipmentType, int quantity)
         {
             this.BuildingId = buildingId;
             this.FloorNumber = floorNumber;
@@ -19,6 +21,8 @@
             this.RoomPurpose = roomPurpose;
             this.WorkingHoursStart = workingHoursStart;
             this.WorkingHoursEnd = workingHoursEnd;
+            this.EquipmentType = equipmentType;
+            this.Quantity = quantity;
         }
 
     }
