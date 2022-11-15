54ae20688a361eb9427e47925964e90ce1a6a1d9
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2022-11-15 22:11:03 +0100

    Add integration background services project and build MQ subscriber process

---
+++ IntegrationServices/MQSubscriberService.cs
@@ -0,0 +1,94 @@
+﻿namespace IntegrationServices
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Hosting;
+    using RabbitMQ.Client;
+    using RabbitMQ.Client.Events;
+
+    internal class MQSubscriberService: BackgroundService
+    {
+        IConnection connection;
+        IModel channel;
+
+        public static String[] MessageParts(string message)
+        {
+            return message.Split(new string[] { '\n' + new String('-', 20) }, StringSplitOptions.None);
+        }
+
+        public static string MessageText(String[] messageParts)
+        {
+            return messageParts[messageParts.Length - 1];
+        }
+
+        public static string MessageTitle(String[] messageParts)
+        {
+            return messageParts[2];
+        }
+
+        public static void MessageImage(String[] messageParts)
+        {
+            // saves the image in the given format
+            string workingDirectory = Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(workingDirectory).FullName;
+
+            string extension = messageParts[0];
+            byte[] imageBytes = Convert.FromBase64String(messageParts[1]);
+
+            string imagePath = Path.Combine(new string[] { projectDirectory, "recievedImage." + extension });
+            var writer = new BinaryWriter(File.OpenWrite(imagePath));
+            writer.Write(imageBytes);
+
+            writer.Flush();
+            writer.Dispose();
+            writer.Close();
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            connection = factory.CreateConnection();
+            channel = connection.CreateModel();
+            channel.QueueDeclare(queue: "hello",
+                                    durable: false,
+                                    exclusive: false,
+                                    autoDelete: false,
+                                    arguments: null);
+
+            // message catching
+            var consumer = new EventingBasicConsumer(channel);  // consumer just consumes basic events
+            consumer.Received += (model, ea) => // init what to do when event hits
+            {
+                var body = ea.Body.ToArray();   // body is byte[]
+                var message = Encoding.UTF8.GetString(body);    // unpack body into string
+
+                var messageParts = MessageParts(message);
+                MessageImage(messageParts);
+            };
+
+            channel.BasicConsume(queue: "hello",    // consume from where
+                                 autoAck: true,     // autoacknowledge
+                                 consumer: consumer);   // who is consuming
+
+
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            channel.Close();
+            connection.Close();
+            return base.StopAsync(cancellationToken);
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ IntegrationServices/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IntegrationServices;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Hosting.WindowsServices;
+
+namespace PrimerServis
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseWindowsService()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<MQSubscriberService>();
+                });
+    }
+}
