f1e8ca0dabe83f65aa0f32fbd0fa73ccf89fdc50
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2022-11-16 08:58:21 +0100

    Expand news service with image handling methods

--- src/IntegrationLibrary/News/Interfaces/INewsService.cs
+++ src/IntegrationLibrary/News/Interfaces/INewsService.cs
@@ -1,8 +1,12 @@
 ﻿namespace IntegrationLibrary.News.Interfaces
 {
     using IntegrationLibrary.Core;
+    using System;
 
     public interface INewsService : IService<News>
     {
+        String GetImageExtension(News entity);
+        String GetImageData(News entity);
+        void SaveImageToDisk(News entity);
     }
 }
