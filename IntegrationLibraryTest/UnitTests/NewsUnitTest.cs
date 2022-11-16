06343e775336f0a28d68fdf69fdeb1159d358d18
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2022-11-16 09:29:20 +0100

    Add unit tests for news service

---
+++ IntegrationLibraryTest/UnitTests/NewsUnitTest.cs
@@ -0,0 +1,49 @@
+﻿namespace IntegrationLibraryTest.UnitTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using IntegrationLibrary.News;
+    using Shouldly;
+    using Moq;
+    using IntegrationLibrary.News.Interfaces;
+
+    public class NewsUnitTest
+    {
+        [Fact]
+        public void Extract_PNG_extension()
+        {
+            var news = new News
+            {
+                Title = "Title",
+                Text = "Body",
+                Image = "png;IMAGE_DATA",
+                Status = NewsStatus.PENDING
+            };
+            var service = new NewsService();
+
+            var result = service.GetImageExtension(news);
+
+            result.ShouldBe("png");
+        }
+
+        [Fact]
+        public void Extract_image_data()
+        {
+            var news = new News
+            {
+                Title = "Title",
+                Text = "Body",
+                Image = "png;IMAGE_DATA",
+                Status = NewsStatus.PENDING
+            };
+            var service = new NewsService();
+
+            var result = service.GetImageData(news);
+
+            result.ShouldBe("IMAGE_DATA");
+        }
+    }
+}
