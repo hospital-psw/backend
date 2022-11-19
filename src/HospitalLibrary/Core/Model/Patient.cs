56a904dfc0f8ffbc3aeebc10ead28be1387cb3e3
Gravarica <93195018+Gravarica@users.noreply.github.com>
2022-11-19 03:43:11 +0100

    Added blood type to patient

--- src/HospitalLibrary/Core/Model/Patient.cs
+++ src/HospitalLibrary/Core/Model/Patient.cs
@@ -1,5 +1,6 @@
 ﻿namespace HospitalLibrary.Core.Model
 {
+    using HospitalLibrary.Core.Model.Blood.Enums;
     using System;
     using System.Collections.Generic;
     using System.Linq;
@@ -9,6 +10,8 @@
     public class Patient : User
     {
         public bool Hospitalized { get; set; }
+
+        public BloodType BloodType { get; set; }
 
         public Patient() { }
 
