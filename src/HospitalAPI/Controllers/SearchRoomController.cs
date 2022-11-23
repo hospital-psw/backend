a39d4baad1a0dc5e14bff7dee04e7cb9b6fd9eb5
Sandra <sandra.brkic5@gmail.com>
2022-11-23 12:04:33 +0100

    Add more values

---
+++ src/HospitalAPI/Controllers/SearchRoomController.cs
@@ -0,0 +1,10 @@
+﻿namespace HospitalAPI.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SearchRoomController
+    {
+    }
+}
