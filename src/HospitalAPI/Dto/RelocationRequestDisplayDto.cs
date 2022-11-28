a432ae9ba7cca223a2687c26cf68959ad42a934a
Sandra <sandra.brkic5@gmail.com>
2022-11-28 15:22:24 +0100

    Add mapper

--- src/HospitalAPI/Dto/RelocationRequestDisplayDto.cs
+++ src/HospitalAPI/Dto/RelocationRequestDisplayDto.cs
@@ -1,25 +1,28 @@
 ﻿namespace HospitalAPI.Dto
 {
+    using HospitalLibrary.Core.Model.Enums;
     using System;
 
     public class RelocationRequestDisplayDto
     {
-        public int FromRoomNumber { get; set; }
-        public int ToRoomNumber { get; set; }
+        public int Id { get; set; }
+        public string FromRoomNumber { get; set; }
+        public string ToRoomNumber { get; set; }
         public int FromRoomId { get; set; }
         public int ToRoomId { get; set; }
-        public int EquipmentId { get; set; }
+        public EquipmentType EquipmentType { get; set; }
         public int Quantity { get; set; }
         public DateTime StartTime { get; set; }
         public int Duration { get; set; }
 
-        public RelocationRequestDisplayDto(int fromRoomNumber, int toRoomNumber, int fromRoomId, int toRoomId, int equipmentId, int quantity, DateTime startTime, int duration)
+        public RelocationRequestDisplayDto(int id, string fromRoomNumber, string toRoomNumber, int fromRoomId, int toRoomId, EquipmentType equipmentType, int quantity, DateTime startTime, int duration)
         {
+            Id = id;
             FromRoomNumber = fromRoomNumber;
             ToRoomNumber = toRoomNumber;
             FromRoomId = fromRoomId;
             ToRoomId = toRoomId;
-            EquipmentId = equipmentId;
+            EquipmentType = equipmentType;
             Quantity = quantity;
             StartTime = startTime;
             Duration = duration;
