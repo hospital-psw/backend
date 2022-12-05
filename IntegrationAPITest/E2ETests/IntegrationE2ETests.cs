a9f78697869eae854a01582dd823d7197eab0128
Ratko Ružičić <ruzicic.ra68.2019@uns.ac.rs>
2022-12-05 20:54:26 +0100

    Setup selenium test env and add bloodbank login e2e test

---
+++ IntegrationAPITest/E2ETests/IntegrationE2ETests.cs
@@ -0,0 +1,48 @@
+﻿namespace IntegrationAPITest.E2ETests
+{
+    using IntegrationAPITest.MockData;
+    using IntegrationAPITest.Setup;
+    using IntegrationLibrary.Settings;
+    using Microsoft.Extensions.DependencyInjection;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using WebDriverManager;
+    using WebDriverManager.DriverConfigs.Impl;
+
+    public class IntegrationE2ETests : BaseIntegrationTest
+    {
+        private ChromeDriver _chromeDriver;
+        public IntegrationE2ETests(TestDatabaseFactory factory) : base(factory) 
+        {
+            var driver = new DriverManager().SetUpDriver(new ChromeConfig());
+            _chromeDriver = new ChromeDriver();
+        }
+        private IntegrationDbContext SetupContext(IServiceScope scope)
+        {
+            var context = scope.ServiceProvider.GetService<IntegrationDbContext>();
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+            context.BloodBanks.Add(BloodBankMockData.BloodBank1);
+            context.SaveChanges();
+            return context;
+        }
+
+        [Fact]
+        public void RegisterBloodBank_PublicApp()
+        {
+            using var scope = Factory.Services.CreateScope();
+            SetupContext(scope);
+            _chromeDriver.Navigate().GoToUrl("http://localhost:4200/changePassword");
+            var emailInput = _chromeDriver.FindElement(By.Id("email"));
+            var passwordInput = _chromeDriver.FindElement(By.Id("password"));
+            var submitButton = _chromeDriver.FindElement(By.Id("submit"));
+
+            emailInput.SendKeys("zika@hotmail.com");
+            passwordInput.SendKeys("4321");
+
+            submitButton.Click();
+
+            Assert.Equal(_chromeDriver.Url, "http://localhost:4200/changePassword");
+        }
+    }
+}
