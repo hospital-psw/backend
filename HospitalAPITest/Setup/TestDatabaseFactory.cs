d78bd13937d173f691cd733bcc3cd0d610fa65fb
Ana Vulin <vulin.ra65.2019@uns.ac.rs>
2022-12-06 19:35:43 +0100

    fixed relocation integration test

--- HospitalAPITest/Setup/TestDatabaseFactory.cs
+++ HospitalAPITest/Setup/TestDatabaseFactory.cs
@@ -512,15 +512,8 @@
                 Room = equipmentRoom
             };
 
-            context.RelocationRequests.Add(new RelocationRequest
-            {
-                FromRoom = relocationFromRoom,
-                ToRoom = relocationToRoom,
-                Equipment = relocationEquipment,
-                StartTime = new DateTime(2022, 12, 10, 23, 0, 0),
-                Duration = 2
-            });
-
+            context.RelocationRequests.Add(RelocationRequest.Create(relocationFromRoom, relocationToRoom, relocationEquipment, 2, new DateTime(2022, 12, 10, 23, 0, 0), 2));
+            
             context.Appointments.Add(new Appointment
             {
                 Date = new DateTime(2023, 12, 25, 12, 0, 0),
