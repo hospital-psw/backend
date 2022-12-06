aef355da888ffeffd62f295c0a96583eb4f92dcc
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2022-12-06 08:59:44 +0100

    Add repository

---
+++ src/IntegrationLibrary/UrgentBloodTransfer/Interfaces/IUrgentBloodTransferRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace IntegrationLibrary.UrgentBloodTransfer.Interfaces
+{
+    using IntegrationLibrary.Core;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IUrgentBloodTransferRepository: IRepository<UrgentBloodTransfer>
+    {
+        UrgentBloodTransfer Get(UrgentBloodTransfer entity);
+        void Delete(UrgentBloodTransfer entity);
+    }
+}
