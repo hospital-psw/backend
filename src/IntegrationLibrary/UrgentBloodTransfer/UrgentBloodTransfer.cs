f4d2a27fd72d64d7b69af583c9bc282f6d5799e5
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2022-12-06 08:44:34 +0100

    Add urgent blood transfer VO

---
+++ src/IntegrationLibrary/UrgentBloodTransfer/UrgentBloodTransfer.cs
@@ -0,0 +1,37 @@
+﻿namespace IntegrationLibrary.UrgentBloodTransfer
+{
+    using grpcServices;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class UrgentBloodTransfer
+    {
+        public BloodType BloodType { get; set; }
+        public uint Amount { get; set; }
+
+        public UrgentBloodTransfer(BloodType bloodType, uint amount)
+        {
+            if (Validate(amount))
+            {
+                BloodType = bloodType;
+                Amount = amount;
+            }
+            else
+                throw new Exception("Cannot create Urgent Blood Transfer object. Amout passed is less than 1");
+        }
+
+        private bool Validate(uint amount)
+        {
+            return amount >= 1;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as UrgentBloodTransfer;
+            return (this.Amount == other.Amount) && (this.BloodType == other.BloodType);
+        }
+    }
+}
