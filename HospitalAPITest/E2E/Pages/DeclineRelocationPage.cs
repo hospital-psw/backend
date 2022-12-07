6586ae9e92948cfeecdbee63f876408038d4b8da
Sandra <sandra.brkic5@gmail.com>
2022-12-07 07:27:39 +0100

    Write selenium test for declining relocation

--- HospitalAPITest/E2E/Pages/DeclineRelocationPage.cs
+++ HospitalAPITest/E2E/Pages/DeclineRelocationPage.cs
@@ -1,12 +1,88 @@
 ﻿namespace HospitalAPITest.E2E.Pages
 {
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Interactions;
+    using OpenQA.Selenium.Support.UI;
     using System;
     using System.Collections.Generic;
+    using System.Collections.ObjectModel;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+    using Xunit.Sdk;
 
-    internal class DeclineRelocationPage
+    public class DeclineRelocationPage
     {
+        private readonly IWebDriver driver;
+        public const string URI = "http://localhost:4200/display";
+
+        private IWebElement canvas => driver.FindElement(By.XPath("/html/body/app-root/app-application-main/div/div[2]/div/app-view-rooms/div/div/div[2]/canvas"));
+        private IWebElement button => driver.FindElement(By.XPath("/html/body/app-root/app-application-main/div/div[2]/div/app-view-rooms/div/div[2]/app-show-room-details/div/app-tabs-details/mat-tab-group/mat-tab[3]/app-relocations/div/table/ng-container[6]/button"));
+        private IWebElement buildingField => driver.FindElement(By.XPath("//*[@id=\"buildingSelect\"]"));
+        private ReadOnlyCollection<IWebElement>  buildingFields => driver.FindElements(By.XPath("//*[@id=\"buildingSelect\"]/mat-option"));
+        private IWebElement buildingFieldBuilding2 => driver.FindElement(By.XPath("//*[@id=\"mat-option-0\"]"));
+        private IWebElement declineRelocationButton => driver.FindElement(By.XPath("//*[@id=\"decline-relocation\"]"));
+        private IWebElement tabsRelocation;
+        private IWebElement tabRelocation;
+        private IWebElement declineButton;
+        public DeclineRelocationPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void Navigate() => driver.Navigate().GoToUrl(URI);
+        public void EnsureTabIsDisplayed()
+        {
+            var wait = new WebDriverWait(driver, new TimeSpan(0, 0, 2000));
+            wait.Until(condition =>
+            {
+                try
+                {
+                    return driver.FindElement(By.XPath("//*[@id=\"mat-tab-label-0-2\"]/div")) != null;//tabsRelocation != null;//driver.FindElement(By.TagName("//*[@id=\"relocationsLabel\"]"));
+                }
+                catch (StaleElementReferenceException)
+                {
+                    return false;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            });
+        }
+
+        public void SelectBuilding()
+        {
+            buildingField.Click();
+            //_ = driver.Manage().Timeouts().ImplicitWait;
+            buildingFieldBuilding2.Click();
+        }
+        public void SelectRoom()
+        {
+            Actions action = new Actions(driver);
+            Thread.Sleep(1000);
+            action.MoveByOffset(897, 415).Click().Perform(); //897 415, 539 260
+            Thread.Sleep(1000);
+            //action.Click();
+        }
+        public void SelectTab()
+        {
+            tabsRelocation = driver.FindElement(By.XPath("//*[@id=\"tabs\"]"));
+            tabRelocation =  driver.FindElement(By.XPath("//*[@id=\"mat-tab-label-0-2\"]/div"));//driver.FindElement(By.XPath("//*[@id=\"relocationsLabel\"]"));
+            tabRelocation.Click();
+            Thread.Sleep(1000);
+        }
+
+        public void Decline()
+        {
+            declineButton = driver.FindElement(By.XPath("//*[@id=\"decline-relocation\"]/span[1]"));
+            declineButton.Click();
+        }
+
+        public int RequestsCount()
+        {
+            return driver.FindElements(By.XPath("//*[@id=\"mat-tab-content-0-2\"]/div/app-relocations/div/table/tbody/tr")).Count;
+        }
+
     }
 }
--- HospitalAPITest/E2E/Tests/DeclineRelocationTest.cs
+++ HospitalAPITest/E2E/Tests/DeclineRelocationTest.cs
@@ -1,12 +1,66 @@
 ﻿namespace HospitalAPITest.E2E.Tests
 {
+    using HospitalAPITest.E2E.Pages;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
     using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
 
-    internal class DeclineRelocationTest
+    public class DeclineRelocationTest
     {
+        private readonly IWebDriver driver;
+        private Pages.DeclineRelocationPage declineRelocationPage;
+        private int requestsCount = 0;
+        //public const string URI_APPOINTMENTS = "http://localhost:4200/appointments";
+
+        public DeclineRelocationTest()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("start-maximized");            // open Browser in maximized mode
+            options.AddArguments("disable-infobars");           // disabling infobars
+            options.AddArguments("--disable-extensions");       // disabling extensions
+            options.AddArguments("--disable-gpu");              // applicable to windows os only
+            options.AddArguments("--disable-dev-shm-usage");    // overcome limited resource problems
+            options.AddArguments("--no-sandbox");               // Bypass OS security model
+            options.AddArguments("--disable-notifications");
+
+
+            driver = new ChromeDriver(options);
+
+
+            declineRelocationPage = new Pages.DeclineRelocationPage(driver);      // create ProductsPage
+            declineRelocationPage.Navigate();                            // navigate to url
+
+        }
+
+        [Fact]
+        public void Test()
+        {
+            ChooseParameters();
+            Decline();
+            requestsCount = declineRelocationPage.RequestsCount();
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            driver.Quit();
+            driver.Dispose();
+        }
+        private void ChooseParameters()
+        {
+            declineRelocationPage.SelectBuilding();
+            declineRelocationPage.SelectRoom();
+            declineRelocationPage.EnsureTabIsDisplayed();
+            declineRelocationPage.SelectTab();
+            requestsCount = declineRelocationPage.RequestsCount();
+        }
+        private void Decline()
+        {
+            declineRelocationPage.Decline();
+        }
     }
 }
