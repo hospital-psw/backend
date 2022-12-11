feb7358f4d027ac2f75a54b7095373ee5c4676c7
Ana Vulin <vulin.ra65.2019@uns.ac.rs>
2022-12-11 13:58:51 +0100

    small change

---
+++ src/HospitalLibrary/Core/Model/ValueObjects/Quantity.cs
@@ -0,0 +1,14 @@
+﻿namespace HospitalLibrary.Core.Model.ValueObjects
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Value;
+
+    public class Quantity : ValueObject
+    {
+
+    }
+}
