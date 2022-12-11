cd65befe05f22d43e9ed8e2c4dd1cdf429363d65
Vuk Milanovic <vuk.milanovic11@gmial.com>
2022-12-11 22:21:41 +0100

    Doctor schedule validation added

--- src/HospitalLibrary/Core/Model/DoctorSchedule.cs
+++ src/HospitalLibrary/Core/Model/DoctorSchedule.cs
@@ -29,5 +29,45 @@
             VacationRequests = vacationRequests;
             Consiliums = consiliums;
         }
+
+        public bool IsDoctorAvailable(DateTime date)
+        {
+            if (IsDoctorOnVacation(date) || DoctorHasAppointment(date) || DoctorHasConsilium(date))
+            {
+                return false;
+            } 
+            return true;
+        }
+
+        private bool IsDoctorOnVacation(DateTime date)
+        {
+            foreach (VacationRequest vr in VacationRequests)
+            {
+                if (DateTime.Compare(vr.From, date) <= 0 && DateTime.Compare(vr.To, date) >= 0)
+                    return true;
+            }
+            return false;
+        }
+
+        private bool DoctorHasAppointment(DateTime dateTime)
+        {
+            foreach (Appointment ap in Appointments)
+            {
+                if (DateTime.Compare(ap.Date, dateTime) == 0)
+                    return true;
+            }
+            return false;
+        }
+
+        private bool DoctorHasConsilium(DateTime dateTime)
+        {
+            foreach (Consilium consilium in Consiliums)
+            {
+                if (DateTime.Compare(consilium.DateTime, dateTime) == 0)
+                    return true;
+            }
+            return false;
+        }
+
     }
 }
