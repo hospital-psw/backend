00cc952ebe501bdd156884311174a4c4bf71156d
Nikola Grbovic <101930180+Grbson00@users.noreply.github.com>
2022-12-13 17:07:46 +0100

    Pzp#3.8 feature/add e2e feedback publication (#129)
    * fix: approve feedback e2e test

--- HospitalAPITest/IntegrationTests/StatisticsIntegrationTest.cs
+++ HospitalAPITest/IntegrationTests/StatisticsIntegrationTest.cs
@@ -36,7 +36,6 @@
 
             Assert.Equal(expected.Chart1, result.Chart1);
             Assert.Equal(expected.Chart2Names, result.Chart2Names);
-            //Assert.Equal(expected.Chart2Values, result.Chart2Values);
             Assert.NotNull(result.Chart2Values);
             Assert.Equal(expected.Chart3Male, result.Chart3Male);
             Assert.Equal(expected.Chart3Female, result.Chart3Female);
