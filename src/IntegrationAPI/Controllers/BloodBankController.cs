72b74981a5b5f5fbc3c3d4135fdba0c63b0fc4e1
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2022-12-14 16:53:57 +0100

    dotnet format

--- src/IntegrationAPI/Controllers/BloodBankController.cs
+++ src/IntegrationAPI/Controllers/BloodBankController.cs
@@ -31,7 +31,7 @@
         {
             return Ok(_mapper.Map<IEnumerable<GetBloodBankDTO>>(_bloodBankService.GetAll()));
         }
-        
+
         [HttpGet("{id}")]
         public virtual IActionResult Get(int id)
         {
