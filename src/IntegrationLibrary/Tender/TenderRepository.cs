8aa192c297cad21d59c2ea2fa6b1854bdb5869a4
Ratko Ružičić <ruzicic.ra68.2019@uns.ac.rs>
2022-12-14 21:02:30 +0100

    Fix Include statements in TenderRepository

--- src/IntegrationLibrary/Tender/TenderRepository.cs
+++ src/IntegrationLibrary/Tender/TenderRepository.cs
@@ -24,15 +24,12 @@ namespace IntegrationLibrary.Tender
 
         public Tender Get(int id)
         {
-            return _context.Set<Tender>().Where(x => x.Id == id && !x.Deleted).Include(x => x.Items).Include(x => x.Offers).FirstOrDefault();
+            return _context.Set<Tender>().Where(x => x.Id == id && !x.Deleted).Include(x => x.Items).Include(x => x.Offers).ThenInclude(x => x.Offeror).Include(x => x.Offers).ThenInclude(x => x.Items).FirstOrDefault();
         }
 
         public IEnumerable<Tender> GetAll()
         {
-            return _context.Set<Tender>()
-                .Include(x => x.Items)
-                .Include(x => x.Offers).ThenInclude(x => x.Items)
-                .Where(x => !x.Deleted).ToList();
+            return _context.Set<Tender>().Where(x => !x.Deleted).Include(x => x.Items).Include(x => x.Offers).ThenInclude(x => x.Offeror).Include(x => x.Offers).ThenInclude(x => x.Items).ToList();
         }
 
         public void Add(Tender entity)
