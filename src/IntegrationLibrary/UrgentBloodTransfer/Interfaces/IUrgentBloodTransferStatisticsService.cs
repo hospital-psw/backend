10f45579b8d309dfb0fa8362c2098480ff6372a9
Ratko Ružičić <ruzicic.ra68.2019@uns.ac.rs>
2022-12-18 19:38:00 +0100

    Declare statistical method prototypes

---
+++ src/IntegrationLibrary/UrgentBloodTransfer/Interfaces/IUrgentBloodTransferStatisticsService.cs
@@ -0,0 +1,13 @@
+﻿namespace IntegrationLibrary.UrgentBloodTransfer.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+
+    public interface IUrgentBloodTransferStatisticsService
+    {        
+        Dictionary<string, Dictionary<string, double>> GetAmountByBloodBankByBloodGroup(DateTime from, DateTime to);
+        Dictionary<string, double> GetBloodBankShare(DateTime from, DateTime to);
+        Dictionary<string, double> GetBloodTypeShare(DateTime from, DateTime to);
+        Dictionary<string, double> GetAmountByBloodUnit(DateTime from, DateTime to);
+    }
+}
---
+++ src/IntegrationLibrary/UrgentBloodTransfer/UrgentBloodTransferStatisticsService.cs
@@ -0,0 +1,29 @@
+﻿namespace IntegrationLibrary.UrgentBloodTransfer
+{
+    using IntegrationLibrary.UrgentBloodTransfer.Interfaces;
+    using System;
+    using System.Collections.Generic;
+
+    public class UrgentBloodTransferStatisticsService : IUrgentBloodTransferStatisticsService
+    {
+        public Dictionary<string, Dictionary<string, double>> GetAmountByBloodBankByBloodGroup(DateTime from, DateTime to)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, double> GetAmountByBloodUnit(DateTime from, DateTime to)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, double> GetBloodBankShare(DateTime from, DateTime to)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, double> GetBloodTypeShare(DateTime from, DateTime to)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
