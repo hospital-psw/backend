6534273d1683c50a51d11b1ebb926a030e198dc0
Sandra <sandra.brkic5@gmail.com>
2023-01-08 09:54:33 +0100

    Fix

--- src/HospitalLibrary/Core/Service/StatisticsService.cs
+++ src/HospitalLibrary/Core/Service/StatisticsService.cs
@@ -368,7 +368,7 @@
 
         public List<double> GetAverageNumberOfRenovationSteps()
         {
-            List<double> retList = new List<double>();//ListFactory.CreateList<double>(0, 0);
+            List<double> retList = new List<double>();
             var list = _renovationService.GetAllSuccessfulAggregates().GroupBy(r => new { ID = r.DateCreated.Year })
                 .Select(g => new { Average = g.Average(p => p.Changes.Count), ID = g.Key.ID });
             foreach(var r in list)
@@ -376,28 +376,6 @@
                 retList.Add(r.Average);
             }
             return retList;
-            /*
-            int renovations2022 = 0;
-            int renovations2023 = 0;
-            foreach (RenovationRequest request in _renovationService.GetAllSuccessfulAggregates())
-            {
-
-                if (request.DateCreated.Year == 2021)
-                {
-                    renovations2022++;
-                    retList[0] += request.Changes.Count;
-                }
-                else if (request.DateCreated.Year == 2022)
-                {
-                    renovations2023++;
-                    retList[1] += request.Changes.Count;
-                }
-            }
-
-            if (renovations2022 > 0) retList[0] = retList[0] / renovations2022;
-            if (renovations2023 > 0) retList[1] = retList[1] / renovations2023;
-            return retList;
-            */
         }
 
         public List<RenovationStatisticDto> GetTimeSpentPerStep()
