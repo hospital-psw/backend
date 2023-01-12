5e3622c74c488e34b1532378994d4539b4b69f1f
SaraJokic <94146847+SaraJokic@users.noreply.github.com>
2023-01-12 10:30:09 +0100

    Value object added

---
+++ src/HospitalLibrary/Core/Model/ValueObjects/AnamnesisDescription.cs
@@ -0,0 +1,31 @@
+﻿namespace HospitalLibrary.Core.Model.ValueObjects
+{
+    using CSharpFunctionalExtensions;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [Owned]
+    public class AnamnesisDescription : ValueObject
+    {
+
+        public string DescriptionContent { get; private set; }
+
+        public AnamnesisDescription(string descriptionContent)
+        {
+            DescriptionContent = descriptionContent;
+        }
+        public static AnamnesisDescription Create(string descriptionContent)
+        {
+            return new AnamnesisDescription(descriptionContent);
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
