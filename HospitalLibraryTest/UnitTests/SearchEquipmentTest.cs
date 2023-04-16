f032a9b6466ecb085c510bb24b6abfee8471d766
Galin-Ilija <ikiakus@gmail.com>
2023-04-16 19:33:03 +0200

    Changed assert section

--- HospitalLibraryTest/UnitTests/SearchEquipmentTest.cs
+++ HospitalLibraryTest/UnitTests/SearchEquipmentTest.cs
@@ -1,5 +1,6 @@
 ﻿namespace HospitalLibraryTest.UnitTests
 {
+    using CSharpFunctionalExtensions;
     using HospitalLibrary.Core.Model;
     using HospitalLibrary.Core.Service;
     using HospitalLibraryTest.InMemoryRepositories;
@@ -9,6 +10,7 @@
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+    using System.Diagnostics;
 
     public class SearchEquipmentTest
     {
@@ -20,6 +22,10 @@
             List<Room> rooms = roomService.Search("003", 0, 4, "ordinacija", new DateTime(2022, 11, 10, 4, 0, 0), new DateTime(2022, 11, 10, 7, 0, 0), 0, 5);
             List<Room> result = equipmentService.SearchRooms(rooms, 0, 5);
             result.ShouldNotBeEmpty();
+            result.Count.ShouldBe(1);
+            result.First().Number.ShouldBe("003");
+            result.First().Purpose.ShouldBe("ordinacija");
+            
         }
         [Fact]
         public void Find_no_suitable_rooms_with_equipment()
@@ -30,7 +36,9 @@
             List<Room> rooms = roomService.Search("003", 0, 4, "ordinacija", new DateTime(2022, 11, 10, 4, 0, 0), new DateTime(2022, 11, 10, 7, 0, 0), 0, 15);
             List<Room> result = equipmentService.SearchRooms(rooms, 0, 15);
 
+            result.ShouldNotBeNull();
             result.ShouldBeEmpty();
+            result.Count.ShouldBe(0);
         }
     }
 }
